d20869d908c0826c6c0d30904fb70beb33922fea
ai-zac <usan31showtek@gmail.com>
2023-05-09 19:53:59 -0600

    Make 1th database model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    firts_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email = models.CharField(max_length=45)
+    password = models.CharField(max_length=100)
+
+
+class ItemsType(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Items(models.Model):
+    id_type = models.ForeignKey(ItemsType, on_delete=models.CASCADE)
+    section = models.CharField(max_length=50)
+    default_price = models.DecimalField(max_digits=5, decimal_places=2,default=0.0, null=True)
+    second_price = models.DecimalField(max_digits=5, decimal_places=2, default=0.0, null=True)
+
+
+class Carts(models.Model):
+    id_user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    items = models.ManyToManyField(Items)
+
+
+class Orders(models.Model):
+    id_cart = models.ForeignKey(Carts, on_delete=models.CASCADE)
+    order = models.BooleanField()
+
