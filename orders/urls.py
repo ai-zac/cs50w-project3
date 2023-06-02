b0aa5a92e60730e82129f8a8e3def08710fd4160
ai-zac <usan31showtek@gmail.com>
2023-06-01 21:39:56 -0600

    Register function: Finish

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
+app_name = "orders"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("accounts/login", views.login, name="login"),
+    path("accounts/register", views.register, name="register"),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,36 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from .models import Plate
+
 
 # Create your views here.
 def index(request):
     context = {
-        "Subs": Plate.objects.filter(section__name="Subs")
+        "Menu": Plate.objects.all(),
     }
     return render(request, "orders/index.html", context)
+
+
+def register(request):
+    inputs = request.POST
+    context = {
+        "UserCreationForm": UserCreationForm(),
+        "isValid": True,
+    }
+
+    if request.method == "POST":
+        form = UserCreationForm(inputs)
+        if form.is_valid():
+            user = User.objects.create_user(
+                inputs["username"], None, inputs["password1"]
+            )
+            user.save()
+            return HttpResponseRedirect(reverse("orders:login"))
+        context["isValid"] = False
+
+    return render(request, "registration/register.html", context)
