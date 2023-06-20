7be0951b8e68172f284b9920f6a227bf817f7b3b
ai-zac <isaacjdv31@gmail.com>
2023-06-19 21:01:03 -0600

    Menu frontend finished

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -10,6 +11,7 @@ from .models import Section
 @login_required(redirect_field_name="")
 def index(request):
     context = {
-        "menu": Section.objects.all(),
+        # Order according to the number of items in each section 
+        "menu": Section.objects.annotate(count=Count("section_plates")).order_by('count'),
     }
     return render(request, "menu/index.html", context)
