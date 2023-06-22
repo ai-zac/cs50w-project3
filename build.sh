56c6828b2ded6a255a51378f56d910fee0bb1d49
ai-zac <isaacjdv31@gmail.com>
2023-06-22 16:17:47 -0600

    rewrite build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,7 @@
 # exit on error
 set -o errexit
 
-pip install --upgrade pip 
-
-pip install poetry
-/opt/render/project/src/.venv/bin/poetry install
+poetry install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
